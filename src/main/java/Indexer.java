e5a420fc7eb0e5a5c57223e39dfa84ba0485233d
alexOlshanskyy <51353059+alexOlshanskyy@users.noreply.github.com>
2021-05-11 23:45:37 -0700

    initial indexer

--- src/main/java/Indexer.java
+++ src/main/java/Indexer.java
@@ -1,6 +1,81 @@
+import java.util.*;
+
 public class Indexer {
 
-    public static void index () {
+    private static final String[] PUNCTUATION = new String[]{"\\.", "\\,", "\\:", "\\;", "\\!", "\\?", "\\[", "\\]", "\"", "\\'", "\\{", "\\}"};
 
+    private static HashMap<String, HashSet<String>> wordToListOfURLs = new HashMap<>();
+    private static HashMap<String, HashMap<String,Integer>> urlToWordCount = new HashMap<>();
+
+    public static void main(String[] args)
+    {
+        index();
+        //System.out.println(wordToListOfURLs);
+        System.out.println(wordToListOfURLs.keySet().size());
+        for (String s : wordToListOfURLs.keySet()){
+
+            System.out.println("Key: " + s);
+            System.out.println("Value: " + wordToListOfURLs.get(s));
+        }
+        //System.out.println(urlToWordCount);
+    }
+
+
+    public static void index() {
+        HashMap<String, HashSet<String>> wordToListOfURLsLocal = new HashMap<>();
+        HashMap<String, HashMap<String,Integer>> urlToWordCountLocal = new HashMap<>();
+        ArrayList<String> domains = FileParser.getDomains();
+        for (String domain : domains) {
+            System.out.println(domain);
+            HashMap<String, String> map = WebCrawler.crawlURL(domain);
+            for (String url: map.keySet()) {
+                HashMap<String, Integer> wordCount = parseWords(map.get(url));
+                urlToWordCountLocal.put(url, wordCount);
+                for (String word: wordCount.keySet()) {
+                    HashSet<String> urls;
+                    if (wordToListOfURLsLocal.containsKey(word)) {
+                        urls = wordToListOfURLsLocal.get(word);
+                    } else {
+                        urls = new HashSet<>();
+                    }
+                    urls.add(url);
+                    wordToListOfURLsLocal.put(word, urls);
+                }
+            }
+        }
+        wordToListOfURLs = wordToListOfURLsLocal;
+        urlToWordCount = urlToWordCountLocal;
+    }
+
+    private static HashMap<String, Integer> parseWords(String words) {
+        HashMap<String, Integer> map = new HashMap<>();
+        //System.out.println("This is words before:" + words);
+        words = cleanUpText(words);
+
+        String[] w = words.split(" ");
+        //System.out.println("This is words after:" + words);
+        //System.out.println(Arrays.toString(w));
+        for (String word : w) {
+            String lowerW = word.toLowerCase();
+            if (word.equals("")) {
+                continue;  // skip multiple spaces in a row
+            }
+            if (map.containsKey(lowerW)) {
+                map.put(lowerW, map.get(lowerW) + 1);
+            } else {
+                map.put(lowerW, 1);
+            }
+        }
+        return map;
+    }
+
+    private static String cleanUpText(String words) {
+        //System.out.println("Start");
+        for (String c : PUNCTUATION) {
+            //System.out.println("1: " + words);
+            words = words.replaceAll(c, " ");
+            //System.out.println("2: " + words);
+        }
+        return words;
     }
 }
--- src/main/java/WebCrawler.java
+++ src/main/java/WebCrawler.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -17,23 +19,45 @@ public class WebCrawler {
 
 
     public static HashMap<String, String> crawlURL(String url) {
+        HashSet<String> linksStrings = new HashSet<>();
+        HashMap<String, String> map = new HashMap<>();
         try {
             Document page = Jsoup.connect(URL_PREFIX + url).get();
             Elements links = page.select("a[href]");
             for (Element e : links) {
                 String path = e.attr("href");
                 // only do internal links
-                if (path.charAt(0) == '/' && path.length() > 1) {
-                    System.out.println(URL_PREFIX + url + path);
+                if (!path.equals("") && path.charAt(0) == '/' && path.length() > 1) {
+                    linksStrings.add(URL_PREFIX + url + path);
                 }
             }
-            String html = page.body().toString();
-            System.out.println(page.body().text());
+            for (String s : linksStrings) {
+                map.put(s, crawlLinkURL(s));
+            }
+
+
+            map.put(URL_PREFIX + url, page.body().text());
+            for (String s : map.keySet()){
+                //System.out.println("Key: " + s);
+                //System.out.println("Value: " + map.get(s));
+            }
+           // System.out.println(map.keySet().size());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return map;
+    }
 
+    private static String crawlLinkURL(String link) {
+        String result = "";
+        try {
+            Document page = Jsoup.connect(link).get();
+            result = page.body().text();
+        } catch (IOException e) {
+             // could happen
+        } finally {
+            return result;
+        }
     }
 
 
